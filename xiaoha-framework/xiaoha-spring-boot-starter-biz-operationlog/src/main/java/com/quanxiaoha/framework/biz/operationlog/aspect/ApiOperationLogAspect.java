33000435d7b5a5c2f61c8b653b4740740a9d5abb
YouleT <youleiye@gmail.com>
2024-05-07 21:58:02 +0800

    Spring Boot 整合 MyBatis

--- xiaoha-framework/xiaoha-spring-boot-starter-biz-operationlog/src/main/java/com/quanxiaoha/framework/biz/operationlog/aspect/ApiOperationLogAspect.java
+++ xiaoha-framework/xiaoha-spring-boot-starter-biz-operationlog/src/main/java/com/quanxiaoha/framework/biz/operationlog/aspect/ApiOperationLogAspect.java
@@ -1,6 +1,6 @@
 package com.quanxiaoha.framework.biz.operationlog.aspect;
 
-import com.quanxiaoha.framework.common.util.JsonUtil;
+import com.quanxiaoha.framework.common.util.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -60,7 +60,7 @@ public class ApiOperationLogAspect {
 
         // 打印出参等相关信息
         log.info("====== 请求结束: [{}], 耗时: {}ms, 出参: {} =================================== ",
-                description, executionTime, JsonUtil.toJsonString(result));
+                description, executionTime, JsonUtils.toJsonString(result));
 
         return result;
     }
@@ -91,7 +91,7 @@ public class ApiOperationLogAspect {
      * @return
      */
     private Function<Object, String> toJsonStr() {
-        return JsonUtil::toJsonString;
+        return JsonUtils::toJsonString;
     }
 
 }
