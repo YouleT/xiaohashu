b0c110b4acb386ecaa3cea376bf46b01beadbd43
YouleT <youleiye@gmail.com>
2024-06-01 00:15:23 +0800

    SaToken 整合 Redis: 解决重启项目，登录失效问题

---
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/service/VerificationCodeService.java
@@ -0,0 +1,18 @@
+package com.quanxiaoha.xiaohashu.auth.service;
+
+import com.quanxiaoha.framework.common.response.Response;
+import com.quanxiaoha.xiaohashu.auth.model.vo.verificationcode.SendVerificationCodeReqVO;
+
+/**
+ * @author youle
+ */
+public interface VerificationCodeService {
+
+    /**
+     * 发送短信验证码
+     *
+     * @param sendVerificationCodeReqVO
+     * @return
+     */
+    Response<?> send(SendVerificationCodeReqVO sendVerificationCodeReqVO);
+}
