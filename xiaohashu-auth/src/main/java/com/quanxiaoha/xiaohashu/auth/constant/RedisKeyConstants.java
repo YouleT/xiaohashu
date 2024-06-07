9a4f5123bb4a5f5bf6a243137bea992df5b6a458
YouleT <youleiye@gmail.com>
2024-06-07 20:26:39 +0800

    整合 Nacos 配置中心：实现动态配置刷新

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/constant/RedisKeyConstants.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/constant/RedisKeyConstants.java
@@ -16,13 +16,28 @@ public class RedisKeyConstants {
     /**
      * 小哈书全局 ID 生成器 KEY
      */
-    public static final String XIAOHASHU_ID_GENERATOR_KEY = "xiaohashu_id_generator";
+    public static final String XIAOHASHU_ID_GENERATOR_KEY = "xiaohashu.id.generator";
 
     /**
      * 用户角色数据 KEY 前缀
      */
     private static final String USER_ROLES_KEY_PREFIX = "user:roles:";
 
+    /**
+     * 角色对应的权限集合 KEY 前缀
+     */
+    private static final String ROLE_PERMISSIONS_KEY_PREFIX = "role:permissions:";
+
+
+    /**
+     * 构建角色对应的权限集合 KEY
+     *
+     * @param roleId
+     * @return
+     */
+    public static String buildRolePermissionsKey(Long roleId) {
+        return ROLE_PERMISSIONS_KEY_PREFIX + roleId;
+    }
 
     /**
      * 构建用户角色 KEY
