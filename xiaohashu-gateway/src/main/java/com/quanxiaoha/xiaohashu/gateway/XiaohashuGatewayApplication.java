dcfab643351a009985227eb72bf472a4dae42fa7
YouleT <youleiye@gmail.com>
2024-06-13 20:01:39 +0800

    网关整合 SaToken: 实现接口鉴权（1）

---
+++ xiaohashu-gateway/src/main/java/com/quanxiaoha/xiaohashu/gateway/XiaohashuGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.quanxiaoha.xiaohashu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author youle
+ */
+@SpringBootApplication
+public class XiaohashuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohashuGatewayApplication.class, args);
+    }
+
+}
