2c087ab5abbe7e7901fff9874facaf1d77cb2870
YouleT <youleiye@gmail.com>
2024-06-19 21:18:17 +0800

    网关过滤器：实现用户 ID 透传到下游服务

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/UserController.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/UserController.java
@@ -7,10 +7,7 @@ import com.quanxiaoha.xiaohashu.auth.service.UserService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: 犬小哈
@@ -30,5 +27,15 @@ public class UserController {
     @ApiOperationLog(description = "用户登录/注册")
     public Response<String> loginAndRegister(@Validated @RequestBody UserLoginReqVO userLoginReqVO) {
         return userService.loginAndRegister(userLoginReqVO);
+    }
+
+    @PostMapping("/logout")
+    @ApiOperationLog(description = "账号登出")
+    public Response<?> logout(@RequestHeader("userId") String userId) {
+        // todo 账号登录逻辑待实现
+
+        log.info("==> 网关透传过来的用户 ID: {}", userId);
+
+        return Response.success();
     }
 }
--- xiaohashu-gateway/src/main/java/com/quanxiaoha/xiaohashu/gateway/constant/RedisKeyConstants.java
+++ xiaohashu-gateway/src/main/java/com/quanxiaoha/xiaohashu/gateway/constant/RedisKeyConstants.java
@@ -1,4 +1,4 @@
-package com.quanxiaoha.xiaohashu.auth.constant;
+package com.quanxiaoha.xiaohashu.gateway.constant;
 
 /**
  * @author: 犬小哈
@@ -8,18 +8,9 @@ package com.quanxiaoha.xiaohashu.auth.constant;
  **/
 public class RedisKeyConstants {
 
-    /**
-     * 验证码 KEY 前缀
-     */
-    private static final String VERIFICATION_CODE_KEY_PREFIX = "verification_code:";
 
     /**
-     * 小哈书全局 ID 生成器 KEY
-     */
-    public static final String XIAOHASHU_ID_GENERATOR_KEY = "xiaohashu.id.generator";
-
-    /**
-     * 用户角色数据 KEY 前缀
+     * 用户对应角色集合 KEY 前缀
      */
     private static final String USER_ROLES_KEY_PREFIX = "user:roles:";
 
@@ -27,7 +18,6 @@ public class RedisKeyConstants {
      * 角色对应的权限集合 KEY 前缀
      */
     private static final String ROLE_PERMISSIONS_KEY_PREFIX = "role:permissions:";
-
 
     /**
      * 构建角色对应的权限集合 KEY
@@ -40,22 +30,12 @@ public class RedisKeyConstants {
     }
 
     /**
-     * 构建用户角色 KEY
+     * 构建用户-角色 KEY
      *
      * @param userId
      * @return
      */
     public static String buildUserRoleKey(Long userId) {
         return USER_ROLES_KEY_PREFIX + userId;
-    }
-
-    /**
-     * 构建验证码 KEY
-     *
-     * @param phone
-     * @return
-     */
-    public static String buildVerificationCodeKey(String phone) {
-        return VERIFICATION_CODE_KEY_PREFIX + phone;
     }
 }
