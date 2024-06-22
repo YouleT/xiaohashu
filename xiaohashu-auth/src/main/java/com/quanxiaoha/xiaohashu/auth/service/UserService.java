153138b4a9aa10520fb44667e8e922fd30acd87f
YouleT <youleiye@gmail.com>
2024-06-22 19:11:37 +0800

    用户退出登录接口开发

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/service/UserService.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/service/UserService.java
@@ -18,4 +18,11 @@ public interface UserService {
      * @return
      */
     Response<String> loginAndRegister(UserLoginReqVO userLoginReqVO);
+
+    /**
+     * 退出登录
+     *
+     * @return
+     */
+    Response<?> logout(Long userId);
 }
